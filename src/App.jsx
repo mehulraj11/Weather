90a0c5da6f03f7d399b7dddc2faa9364028c282c
mehulraj11 <mailtomehulraj@gmail.com>
2024-06-17 15:31:27 +0530

    fetching data through api is working, no UI, not able to show handle error when undefined search is encountered

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const API_KEY = "4d3ef14266c14f5d20be5aeb257a59b0";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputSearch, setInputSearch] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleChange = (e) => {
+    setInputSearch(e.target.value);
+  };
+
+  const handleClick = async (e) => {
+    try {
+      e.preventDefault();
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputSearch}&appid=${API_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setInputSearch("");
+      setWeatherData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const firstRender = async () => {
+      try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Patna&appid=${API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    firstRender();
+  }, []);
 
   return (
     <>
+      <form>
+        <input type="text" value={inputSearch} onChange={handleChange} />
+        <button onClick={handleClick}>Search</button>
+      </form>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {weatherData && (
+          <div>
+            <h2>Weather in {weatherData.name}</h2>
+            <p>Temperature: {(weatherData.main.temp - 273.15).toFixed(2)} °C</p>
+            <p>Weather: {weatherData.weather[0].description}</p>
+            <p>Humidity: {weatherData.main.humidity}%</p>
+            <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
