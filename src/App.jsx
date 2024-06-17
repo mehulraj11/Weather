572145418e57f29468b1022e63cc76c7d8e8cb29
mehulraj11 <mailtomehulraj@gmail.com>
2024-06-17 20:03:28 +0530

    city not found fixed, loading screen created

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,48 @@ const API_KEY = "4d3ef14266c14f5d20be5aeb257a59b0";
 function App() {
   const [inputSearch, setInputSearch] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [notFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleChange = (e) => {
     setInputSearch(e.target.value);
   };
 
   const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
     try {
-      e.preventDefault();
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputSearch}&appid=${API_KEY}`;
       const response = await fetch(url);
       const data = await response.json();
       setInputSearch("");
-      setWeatherData(data);
+      if (data.cod === 200) {
+        setNotFound(false);
+        setWeatherData(data);
+      } else {
+        setNotFound(true);
+        setWeatherData(null);
+      }
+      console.log(data.cod);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     const firstRender = async () => {
+      setLoading(true);
       try {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=Patna&appid=${API_KEY}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Muzaffarpur&appid=${API_KEY}`;
         const response = await fetch(url);
         const data = await response.json();
         setWeatherData(data);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -45,15 +60,24 @@ function App() {
         <input type="text" value={inputSearch} onChange={handleChange} />
         <button onClick={handleClick}>Search</button>
       </form>
+
       <div>
-        {weatherData && (
-          <div>
-            <h2>Weather in {weatherData.name}</h2>
-            <p>Temperature: {(weatherData.main.temp - 273.15).toFixed(2)} °C</p>
-            <p>Weather: {weatherData.weather[0].description}</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
-            <p>Wind Speed: {weatherData.wind.speed} m/s</p>
-          </div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : notFound ? (
+          <div>CITY NOT FOUND</div>
+        ) : (
+          weatherData && (
+            <div>
+              <h2>Weather in {weatherData.name}</h2>
+              <p>
+                Temperature: {(weatherData.main.temp - 273.15).toFixed(2)} °C
+              </p>
+              <p>Weather: {weatherData.weather[0].description}</p>
+              <p>Humidity: {weatherData.main.humidity}%</p>
+              <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+            </div>
+          )
         )}
       </div>
     </>
